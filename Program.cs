60357b3e41a394b18fd217822d2eb7af87ac3f00
LEONARDO BALITSKI FILIPINI <leonardo.filipini@alunos.sc.senac.br>
2020-02-11 22:13:28 -0300

    primeiro up

---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace c__dale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+   /*  Double numero1= 10;
+    Double numero2= 20;
+    Double numero3= 30;*/
+
+    /* int lado1 = 10;
+    int lado2 = 20;*/
+
+ /* double media = (numero1 + numero2 + numero3) / 3;
+
+ Console.WriteLine("o numero  é = " + media);
+
+ double area = (numero1 * numero2 );
+
+ Console.WriteLine("A area é = " + area);*/
+
+    /* int largura1 = 5;
+
+    int altura2 = 2;
+
+    int profundidade = 10;
+
+    double volume = (largura1 * altura2 * profundidade);
+
+ Console.WriteLine("o volume é = " + volume);*/
+
+
+    /* int a = 50;
+    int b = 20;
+
+    if(a > (b*b)){
+        Console.WriteLine("esse é maior");
+    } else {
+        
+        Console.WriteLine("este valoe não e maior que o dobro do outro filhão");
+    }*/
+
+
+    /*int a = 5;
+    int b = 3;
+    int c = 8;
+    
+    double delta, x1, x2;
+
+    delta = (b * b) + (-4 *(a * c));
+     Console.WriteLine($"Delta é: {delta}");
+    
+     if(delta >= 0){
+         x1 = (-b +Math.Sqrt(delta)) / (2 * a);
+         x2 = (-b +Math.Sqrt(delta)) / (2 * a);
+     
+     Console.WriteLine(" x1 =" + x1);
+     Console.WriteLine(" x2 =" + x2);
+
+     } else {
+         Console.WriteLine("Delta não possui raiz");
+     }*/
+
+    /*float dist6 = 300;
+    float tempo6 = 3;
+    float veloc6;
+
+    veloc6 = dist6 / tempo6;
+
+    Console.WriteLine("velocidade media é = " + veloc6 + "km/h");*/
+
+
+        /*float faturamento = 100000;
+        float perc = 50;
+        float imposto;
+
+        imposto = (faturamento * perc) / 100;  
+
+        Console.WriteLine("Resposta R$ = " + imposto);*/
+
+        /*int num1 = 2;
+
+        if(num1 < 0) {
+             Console.WriteLine("object numero é negativo");
+        }
+
+        if( num1 %2 == 0) {
+             Console.WriteLine(" resposta = " + num1 + " é par");
+        } else {
+            Console.WriteLine(" resposta = " + num1 + " é Impar");
+        }*/
+
+        /* string leo = @"C:\users";
+        string filipini = @"C:\Users";
+
+
+
+        if(leo.Equals(filipini)){
+            Console.WriteLine( "é igual");
+        } else {
+    Console.WriteLine("não é igual");
+        }*/
+
+        /*double salarioM = 3500.00;
+        double porc12 = salarioM <= 1903.00
+
+        ? 0
+        : salarioM >= 1903.66 && salarioM <= 2826.68
+        ? 7.5
+        : salarioM >= 2826.06 && salarioM <= 3751.68
+        ? 15
+        : salarioM >= 3751.06 && salarioM <= 4664.68
+        ? 22.5 : 27.5;
+
+        Console.WriteLine("Resposta piazão");
+        Console.WriteLine(" Salario é: r$ " + salarioM.ToString("C"));
+        Console.WriteLine(" aliquota do IR é:  " + porc12 + "%");*/
+//leo
+        }
+    }
+}
+
+
+
+    
+
